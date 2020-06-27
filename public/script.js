49cfd09a612393d33015bef82f50601d8fbf9e69
Gamel James <g_james@apple.com>
2020-06-27 02:49:52 -0700

    Cart and searchbar Updated, added vue resources

--- public/script.js
+++ public/script.js
@@ -3,15 +3,51 @@ new Vue({
 	data: {
 		total: 0,
 		products: [
-			{ title: "product 1", id: 1 },
-			{ title: "product 2", id: 2 },
-			{ title: "product 3", id: 3 },
+			{ title: "product 1", id: 1, price: 9.99 },
+			{ title: "product 2", id: 2, price: 9.99 },
+			{ title: "product 3", id: 3, price: 9.99 },
 		],
 		cart: [],
+		search:"",
 	},
 	methods: {
 		addToCart: function (product) {
-			console.log(product.id);
+			this.total += product.price;
+			var found = false;
+			for (var i = 0; i < this.cart.length; i++) {
+				if (this.cart[i].id === product.id) {
+					this.cart[i].qty++;
+					found = true;
+				}
+			}
+			if (!found) {
+				this.cart.push({
+					id: product.id,
+					title: product.title,
+					price: product.price,
+					qty: 1,
+				});
+			}
+		},
+		inc: function (item) {
+			item.qty++;
+			this.total += item.price;
+		},
+		dec: function (item) {
+			item.qty--;
+			this.total -= item.price;
+			if (item.qty <= 0) {
+				var i = this.cart.indexOf(item);
+				this.cart.splice(i,1);
+			}
+		},
+		onSubmit:function(){
+			console.log('Submit')
+		}
+	},
+	filters: {
+		currency: function (price) {
+			return "$".concat(price.toFixed(2));
 		},
 	},
 });
