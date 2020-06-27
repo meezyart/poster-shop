1c7e558dfe07479e6a8736ffd3423731596b1c87
Gamel James <g_james@apple.com>
2020-06-27 01:25:27 -0700

    add to cart Button

--- public/script.js
+++ public/script.js
@@ -1,1 +1,17 @@
-console.log("It works!");
+new Vue({
+	el: "#app",
+	data: {
+		total: 0,
+		products: [
+			{ title: "product 1", id: 1 },
+			{ title: "product 2", id: 2 },
+			{ title: "product 3", id: 3 },
+		],
+		cart: [],
+	},
+	methods: {
+		addToCart: function (product) {
+			console.log(product.id);
+		},
+	},
+});
